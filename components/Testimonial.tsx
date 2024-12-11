84a3fddfdd682bec44f1a8af4e611937fc5b8d89
Patricio Avila <p.avil;af1998@gmail.com>
2024-12-11 01:12:31 -0300

    Setting up the testimonial component

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import { TESTIMONIAL } from "@/constant";
+import React from "react";
+import Slider from "react-slick";
+import { RiDoubleQuotesR } from "react-icons/ri";
+import Image from "next/image";
 
 const Testimonial = () => {
+
+    let settings = {
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1280,
+            settings: {
+              slidesToShow: 4,
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+            },
+          },
+        ],
+      };
+
+  return (
+    <section className="padding-container max-container pt-16 bg-slate-10 realative">
+      <h3 className="bold-32 text-center">Testimonios</h3>
+      <p className="text-center max-lg m-auto text-gray-30 py-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel assumenda
+        in ut praesentium doloremque eos fugit beatae obcaecati amet voluptate
+        sit amet consectetur adipisicing elit.
+      </p>
+      <Slider {...settings}>
+        {
+            TESTIMONIAL.map((testimonial) => (
+                <TestimonialItem
+                key={testimonial.title}
+                title={testimonial.title}
+                des={testimonial.des}
+                profession={testimonial.profesion} 
+                URL={testimonial.URL}/>
+            ))
+        }
+      </Slider>
+    </section>
+  );
+};
+
+type TestimonialItem = {
+    des: string;
+    title: string;
+    URL: string;
+    profession: string;
+}
+
+
+const TestimonialItem = ({ title, profession, URL, des }: TestimonialItem) => {
     return (
-        <div>Testimonial</div>
+        <div className="mx-4 my-12 relative px-8 py-12 rounded-md bg-white z-10">
+            <span className="text-slate-10 text-9xl absolute -top-4 right-4 -z-10">
+             <RiDoubleQuotesR/>
+             </span>
+            <p className="text-gray-50 text-center">{des}</p>
+            <div className="flexCenter gap-4 mt-8">
+            <Image
+            src={URL}
+            alt='user'
+            height={55}
+            width={55}
+            className='rounded-full'
+            />
+            </div>
+            <div className="medium-14">{title}</div>
+            <div className="text-gray-20 regular-14">{profession}</div>
+
+        </div>
     )
 }
 
-export default Testimonial
+export default Testimonial;
--- constant/index.ts
+++ constant/index.ts
@@ -137,4 +137,36 @@ export const NAV_LINKS = [
 
         ]
 
+        export const TESTIMONIAL = [
+            {
+                title: "James Smith",
+                profesion: "Director CEO",
+                URL: "/assets/person-1.jpg",
+                des: "Fantásticos destinos, guías con conocimientos, logística impecable, espectaculares vistas y recuerdos. Excedió mis expectativas; valió cada centavo."
+            },
+            {
+                title: "Sophia Johnson",
+                profesion: "Diseñadora Gráfica",
+                URL: "/assets/person-2.jpg",
+                des: "Una experiencia inolvidable. La organización fue impecable y los lugares visitados, impresionantes. Totalmente recomendable."
+            },
+            {
+                title: "Michael Brown",
+                profesion: "Ingeniero de Software",
+                URL: "/assets/person-3.jpg",
+                des: "Todo fue perfecto, desde la planificación hasta la ejecución. Los guías fueron muy amables y profesionales. ¡Volveré!"
+            },
+            {
+                title: "Emily Davis",
+                profesion: "Chef Internacional",
+                URL: "/assets/person-4.jpg",
+                des: "Cada detalle estuvo cuidado al máximo. Disfruté mucho de los paisajes y las actividades organizadas. Una experiencia única."
+            },
+            {
+                title: "David Wilson",
+                profesion: "Músico Profesional",
+                URL: "/assets/person-5.jpg",
+                des: "Me sorprendió la calidad de los servicios ofrecidos. Los paisajes y la atención personalizada hicieron que este viaje fuera inolvidable."
+            }
+        ];
         
